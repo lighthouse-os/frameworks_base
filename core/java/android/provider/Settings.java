d3c96f7044a9db4ce75851fa44454ee1d98aa117
Jon Haus <haus.xda@gmail.com>
2017-01-05 00:20:06 +0100

    base: Network Traffic [1/2]
    -----
    abc ezio84:
    use O darkintensity api
    use arrows vectors from aosp net arrows commit (thanks @Mazda-- for this find!): https://github.com/ezio84/abc_frameworks_base/commit/ad71ae1fd09f9a7392ab2eba7b766c8b088319b4
    fix visual glitch on statusbar expand (arrows showing and quickly hiding again)
    don't show arrows when no data
    remove unit settings (KB is good)
    remove interval (set to 1sec by def) and single arrow options
    don't get settings values again when not needed
    fix padding between arrows and speed values and expose the padding value
    code cleanup
    -----
    
    Change-Id: I026f67caec69a09c0c1969a09fbe99aa6b6f0510
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5505,6 +5505,18 @@ public final class Settings {
         public static final String LESS_BORING_HEADS_UP = "less_boring_heads_up";
 
         /**
+         * Whether to show network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
+        /**
+         * Network traffic inactivity threshold (default is 1 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
