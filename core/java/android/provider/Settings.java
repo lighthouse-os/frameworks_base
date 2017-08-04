c9ad356e0ef32420e678c4304f88845b463b10d4
LorDClockaN <lordclockan@gmail.com>
2017-08-04 15:43:57 +0200

    base: Sensor block per-package switch (1/2)
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Change-Id: I35413fb00d60f3ab11d0039b1cc3a7de799b2626

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4905,6 +4905,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Sensor block per-package
+         * @hide
+         */
+        public static final String SENSOR_BLOCK = "sensor_block";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
