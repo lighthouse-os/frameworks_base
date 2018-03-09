4524b0e4777e35454ad5f8897ea5c6b93a53fee9
carlosavignano <carlosavignano@aospa.co>
2018-03-09 04:08:46 +0000

    Keyguard: Hide check button when using PIN quick unlock
    Change-Id: I6426bd4bf79fbb5447d337489d086eaf0a3d802b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4966,6 +4966,17 @@ public final class Settings {
          * 3 - half top of the screen
          * @hide
          */
+        public static final String EDGE_GESTURE_Y_DEAD_ZONE = "edge_gesture_y_dead_zone";
+
+        /**
+         * Keys we no longer back up under the current schema, but want to continue to
+         * process when restoring historical backup datasets.
+         *
+         *
+         * @hide
+         */
+        public static final String[] LEGACY_RESTORE_SETTINGS = {
+        };
 
 
         /**
