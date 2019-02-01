10e0419e768d3c7ae94002dfd4b9553902290cbb
Marko Man <darkobas@gmail.com>
2019-02-01 15:43:13 +0100

    [1/2] base: add option to enable AOD on charging only
    Change-Id: I95bf93e7902e9cedc02726403b9a6ab111c7c669
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5219,6 +5219,18 @@ public final class Settings {
          */
         public static final String POCKET_JUDGE = "pocket_judge";
 
+         /**
+         * Whether to enable DOZE only when charging
+         * @hide
+         */
+        public static final String DOZE_ON_CHARGE = "doze_on_charge";
+
+        /**
+         * hidden stting of the current atate of DOZE only when charging
+         * @hide
+         */
+        public static final String DOZE_ON_CHARGE_NOW = "doze_on_charge_now";
+
         /**
          * Whether to show battery percent on ambient bottom
          * @hide
