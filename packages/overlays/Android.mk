194b3d140a645101474b99234e04e5b7c54a34f9
Joey Huab <joey@evolution-x.org>
2020-05-16 04:05:44 +0000

    overlays: Accents: Add Pixel Blue
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -38,6 +38,7 @@ LOCAL_REQUIRED_MODULES := \
 	AccentColorOnePlusOverlay \
 	AccentColorParanoidOverlay \
 	AccentColorPepsiOverlay \
+	AccentColorPixelBlueOverlay \
 	AccentColorRazerOverlay \
 	AccentColorStarbucksOverlay \
 	AccentColorXboxOverlay \
