7e6308851a582f9b9aff96e23e083a623bf3f17c
Joey Huab <joey@evolution-x.org>
2020-12-01 06:50:16 +0900

    SystemUI: FOD pressed colors [1/2]
    @Stallix: Adapt for A12/UDFPS changes.
    
    Use TunerService API. Thanks to @neobuddy89 for helping out as well!
    
    Includes the following:
    
    * https://github.com/Evolution-X/frameworks_base/commit/b70ac04e293c7a2eea2710a1a689630a74e70364
    * https://github.com/Evolution-X/frameworks_base/commit/6ecbadff3550c01682a15331f3ccb0abf614d845
    * https://github.com/Evolution-X/frameworks_base/commit/ea6a283ed691c885ee2e0d5c63f0eccc10f1e228
    * https://github.com/Evolution-X/frameworks_base/commit/fc31385f977936c38039b9e04fe98c394be2ca91
    * https://github.com/Evolution-X/frameworks_base/commit/83df035f986311e6e890a3bc3c8081486c5c71d3
    * https://github.com/Evolution-X/frameworks_base/commit/70d980525ba293d827811c522762bad14794866d
    * https://github.com/Evolution-X/frameworks_base/commit/3d2b553558f7a0cc83ecb69971d50c858ba59304
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Akito Mizukito <akito@evolution-x.org>
    Co-authored-by: Marco Rizzo <marco.rizzo.00@outlook.com>
    Co-authored-by: Varun Date <date.varun123@gmail.com>
    Co-authored-by: firebird11 <hbgassel@gmail.com>
    Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5553,6 +5553,12 @@ public final class Settings {
         public static final String FP_ERROR_VIBRATE = "fingerprint_error_vib";
 
         /**
+         * FOD pressed color
+         * @hide
+         */
+        public static final String FOD_COLOR = "fod_color";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
