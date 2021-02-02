a9022de2ec27e7dd9f396cd2e4ad57c20393dd1d
Akash Srivastava <akashniki@gmail.com>
2021-02-02 23:26:06 +0000

    Clock: Introduce 404 IDE Clock
    Layout cleanup help by @ jyotiradityaPanda
    
    Co-authored-by: Rodolphe06 <lokiikol@hotmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -163,6 +163,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new BinaryClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new TypeClockAltController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new SfunyClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new IDEClockController(res, layoutInflater, colorExtractor));
 
 
         // Store the size of the display for generation of clock preview.
