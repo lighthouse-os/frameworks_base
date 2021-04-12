8d4fa0e545814ca0272435e21e94ea77753d8929
Ido Ben-Hur <idoybh2@gmail.com>
2021-04-12 17:26:05 +0300

    SystemUI: Make running services icon gone instead of invisible
    Fixes weird padding if it's disabled while multi user is enabled
    
    Change-Id: I1f6d3c2982b47c93881155f3cef4e4886a3b3b72
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -290,7 +290,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
         mMultiUserSwitch.setVisibility(showUserSwitcher() ? View.VISIBLE : View.INVISIBLE);
         mEditContainer.setVisibility(isDemo || !mExpanded ? View.INVISIBLE : View.VISIBLE);
         mSettingsButton.setVisibility(isDemo || !mExpanded ? View.INVISIBLE : View.VISIBLE);
-        mRunningServicesButton.setVisibility(!isDemo && mExpanded ? View.VISIBLE : View.INVISIBLE);
+        mRunningServicesButton.setVisibility(!isDemo && mExpanded ? View.VISIBLE : View.GONE);
     }
 
     private boolean showUserSwitcher() {
