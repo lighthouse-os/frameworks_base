2cb87060b628616f58af041cada3c879732a230f
CannedShroud <hypersync112@gmail.com>
2021-05-06 19:18:01 +0530

    Revert "Initial import of lineagehw"
    This reverts commit 53766b6dfdca78f6ddada01b36e98b94d64d5c70.

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -193,9 +193,6 @@ import java.util.Timer;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 
-// LineageHardware
-import com.android.server.custom.LineageHardwareService;
-
 public final class SystemServer {
 
     private static final String TAG = "SystemServer";
@@ -2018,13 +2015,6 @@ public final class SystemServer {
             t.traceBegin("StartPeopleService");
             mSystemServiceManager.startService(PeopleService.class);
             t.traceEnd();
-
-            // LineageHardware
-            if (!mOnlyCore){
-                t.traceBegin("StartLineageHardwareService");
-                mSystemServiceManager.startService(LineageHardwareService.class);
-                t.traceEnd();
-            }
         }
 
         if (!isWatch) {
