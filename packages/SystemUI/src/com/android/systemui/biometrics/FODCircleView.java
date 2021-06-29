LineageOS/android_frameworks_base

FODCircleView: Restore DIM_BEHIND and dim amount to 0.0f on base view
* This will fix problem onTouch of apparently boost brightness for Xiaomi Displayfeature FOD implementation

Change-Id: Ia29e048f09fb5293e8ca77d36e2a2707f563459b

packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -229,7 +229,8 @@ public class FODCircleView extends ImageView {
         mParams.type = WindowManager.LayoutParams.TYPE_DISPLAY_OVERLAY;
         mParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
                 WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN |
-                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH;
+                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH |
+                WindowManager.LayoutParams.FLAG_DIM_BEHIND;
         mParams.gravity = Gravity.TOP | Gravity.LEFT;
 
         mPressedParams.copyFrom(mParams);
@@ -238,6 +239,8 @@ public class FODCircleView extends ImageView {
 
         mParams.setTitle("Fingerprint on display");
         mPressedParams.setTitle("Fingerprint on display.touched");
+
+        mParams.dimAmount = 0.0f;
 
         mPressedView = new ImageView(context)  {
             @Override

lighthouse-os/frameworks_base

Signed-off-by: Joey Huab <joey@evolution-x.org>

packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -240,7 +240,8 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         mParams.packageName = "android";
         mParams.type = WindowManager.LayoutParams.TYPE_DISPLAY_OVERLAY;
         mParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
-                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN |
+                WindowManager.LayoutParams.FLAG_DIM_BEHIND;
         mParams.gravity = Gravity.TOP | Gravity.LEFT;
 
         mPressedParams.copyFrom(mParams);
@@ -249,6 +250,8 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         mParams.setTitle("Fingerprint on display");
         mPressedParams.setTitle("Fingerprint on display.touched");
 
+        mParams.dimAmount = 0.0f;
+        
         mPressedView = new ImageView(context)  {
             @Override
             protected void onDraw(Canvas canvas) {