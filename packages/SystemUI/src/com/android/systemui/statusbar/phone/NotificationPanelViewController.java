a131388b07dbe3717440c1efd78fd536aec281d9
CannedShroud <hypersync112@gmail.com>
2021-11-11 14:33:37 +0530

    base: NotificationPanelViewController: Fix context
    Signed-off-by: CannedShroud <hypersync112@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -858,7 +858,7 @@ public class NotificationPanelViewController extends PanelViewController {
                 new GestureDetector.SimpleOnGestureListener() {
             @Override
             public boolean onDoubleTap(MotionEvent e) {
-                LighthouseUtils.switchScreenOff(context);
+                LighthouseUtils.switchScreenOff(mView.getContext());
                 return true;
             }
         });
