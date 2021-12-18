82e8614177847417c26d0da7a34820bb1d553f03
Stealth1226 <rishawniyer06@gmail.com>
2021-12-18 14:21:11 +0000

    Revert "keystore: Block key attestation for SafetyNet"
    This reverts commit 1cd47b1933d598490cd41a46b6c9646d414fff28.

--- core/java/com/android/internal/gmscompat/AttestationHooks.java
+++ core/java/com/android/internal/gmscompat/AttestationHooks.java
@@ -22,7 +22,6 @@ import android.os.SystemProperties;
 import android.util.Log;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
 
 /** @hide */
 public final class AttestationHooks {
@@ -30,8 +29,6 @@ public final class AttestationHooks {
 
     private static final String PRODUCT_STOCK_FINGERPRINT =
             SystemProperties.get("ro.build.stock_fingerprint");
-
-    private static volatile boolean sIsGms = false;
 
     private AttestationHooks() { }
 
@@ -63,20 +60,7 @@ public final class AttestationHooks {
 
     public static void initApplicationBeforeOnCreate(Application app) {
         if (GmsInfo.PACKAGE_GMS.equals(app.getPackageName())) {
-            sIsGms = true;
             spoofBuildGms();
-        }
-    }
-
-    private static boolean isCallerSafetyNet() {
-        return Arrays.stream(Thread.currentThread().getStackTrace())
-                .anyMatch(elem -> elem.getClassName().contains("DroidGuard"));
-    }
-
-    public static void onEngineGetCertificateChain() {
-        // Check stack for SafetyNet
-        if (sIsGms && isCallerSafetyNet()) {
-            throw new UnsupportedOperationException();
         }
     }
 }
