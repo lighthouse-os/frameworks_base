eccb5d37ba4f86941451845c0e27c8e19eec85c4
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:49:21 +0530

    SystemUI: Initial Lighthouse SystemUI stub
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I67f36f20cc01dfc82572f2241287c3e739592306
    Signed-off-by: DennySPb <dennyspb@gmail.com>

---
+++ packages/SystemUI/src/com/lighthouse/android/systemui/dagger/LighthouseSysUISubcomponentModule.java
@@ -0,0 +1,10 @@
+package com.lighthouse.android.systemui.dagger;
+
+import dagger.Module;
+
+/**
+ * Dagger module for including the WMComponent.
+ */
+@Module(subcomponents = {LighthouseSysUIComponent.class})
+public abstract class LighthouseSysUISubcomponentModule {
+}
