d75105b1e1e5275ad2ff5d500c0b5077db6e1a62
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 08:31:51 +0530

    SystemUI: Integrate smartspace and backgesture provider
    Change-Id: Iadaaa3e24b3a30edeff59e0224d063bf71e20d3d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: alanndz <alanndz7@gmail.com>
    Signed-off-by: CannedShroud <hypersync112@gmail.com>

--- packages/SystemUI/src/com/lighthouse/systemui/LighthouseSystemUIFactory.java
+++ packages/SystemUI/src/com/lighthouse/systemui/LighthouseSystemUIFactory.java
@@ -1,9 +1,19 @@
 package com.lighthouse.systemui;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Handler;
 
 import com.lighthouse.systemui.dagger.LighthouseGlobalRootComponent;
+import com.lighthouse.systemui.dagger.LighthouseSysUIComponent;
 import com.lighthouse.systemui.dagger.DaggerLighthouseGlobalRootComponent;
+import com.android.systemui.navigationbar.gestural.BackGestureTfClassifierProvider;
+import com.android.systemui.screenshot.ScreenshotNotificationSmartActionsProvider;
+
+import com.google.android.systemui.gesture.BackGestureTfClassifierProviderGoogle;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 import com.android.systemui.SystemUIFactory;
 import com.android.systemui.dagger.GlobalRootComponent;
@@ -14,5 +24,18 @@ public class LighthouseSystemUIFactory extends SystemUIFactory {
         return DaggerLighthouseGlobalRootComponent.builder()
                 .context(context)
                 .build();
+    }
+
+    @Override
+    public BackGestureTfClassifierProvider createBackGestureTfClassifierProvider(AssetManager am, String modelName) {
+        return new BackGestureTfClassifierProviderGoogle(am, modelName);
+    }
+
+    @Override
+    public void init(Context context, boolean fromTest) throws ExecutionException, InterruptedException {
+        super.init(context, fromTest);
+        if (shouldInitializeComponents()) {
+            ((LighthouseSysUIComponent) getSysUIComponent()).createKeyguardSmartspaceController();
+        }
     }
 }
