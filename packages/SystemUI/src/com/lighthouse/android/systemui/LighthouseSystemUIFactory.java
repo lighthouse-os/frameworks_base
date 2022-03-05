9fc6e736418a71cb5ed6274452c545a2eb1aa0ae
jhonboy121 <alfredmathew05@gmail.com>
2022-03-04 16:19:46 -0800

    SystemUI: Cleanup smart space
    * I'm too lazy to write what I did now, kinda self-explanatory. Removed deprecated asynctask, unused imports, blah blah blah
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I19f77817722a276c623859e51728d06f959b4fa9
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/lighthouse/android/systemui/LighthouseSystemUIFactory.java
+++ packages/SystemUI/src/com/lighthouse/android/systemui/LighthouseSystemUIFactory.java
@@ -2,7 +2,6 @@ package com.lighthouse.android.systemui;
 
 import android.content.Context;
 import android.content.res.AssetManager;
-import android.os.Handler;
 
 import com.lighthouse.android.systemui.dagger.LighthouseGlobalRootComponent;
 import com.lighthouse.android.systemui.dagger.LighthouseSysUIComponent;
