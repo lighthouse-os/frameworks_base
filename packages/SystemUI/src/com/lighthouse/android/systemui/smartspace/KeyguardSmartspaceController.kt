Octavi-OS/platform_frameworks_base

[Squash] Add SystemUI Stub along with smartspace and backgesture provider
- Squash of following commits :-
SystemUI: Initial SystemUI stub
SystemUI: update monet according to new sysui stub
SystemUI: integrate smartspace and backgesture provider

packages/SystemUI/src/com/octavi/android/systemui/smartspace/KeyguardSmartspaceController.kt
@@ -1,0 +1,29 @@
+package com.octavi.android.systemui.smartspace
+
+import android.content.ComponentName
+import android.content.Context
+
+import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.statusbar.FeatureFlags
+
+import com.google.android.systemui.smartspace.KeyguardMediaViewController
+import com.google.android.systemui.smartspace.KeyguardZenAlarmViewController
+
+import javax.inject.Inject
+
+@SysUISingleton
+class KeyguardSmartspaceController @Inject constructor(
+    private val context: Context,
+    private val featureFlags: FeatureFlags,
+    private val zenController: KeyguardZenAlarmViewController,
+    private val mediaController: KeyguardMediaViewController,
+) {
+    init {
+        if (!featureFlags.isSmartspaceEnabled()) {
+            context.packageManager.setComponentEnabledSetting(ComponentName("com.android.systemui", "com.octavi.android.systemui.keyguard.KeyguardSliceProviderOctavi"), 1, 1)
+        } else {
+            mediaController.init()
+            zenController.init()
+        }
+    }
+}

lighthouse-os/frameworks_base

packages/SystemUI/src/com/lighthouse/android/systemui/smartspace/KeyguardSmartspaceController.kt
@@ -1,0 +1,29 @@
+package com.lighthouse.android.systemui.smartspace
+
+import android.content.ComponentName
+import android.content.Context
+
+import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.statusbar.FeatureFlags
+
+import com.google.android.systemui.smartspace.KeyguardMediaViewController
+import com.google.android.systemui.smartspace.KeyguardZenAlarmViewController
+
+import javax.inject.Inject
+
+@SysUISingleton
+class KeyguardSmartspaceController @Inject constructor(
+    private val context: Context,
+    private val featureFlags: FeatureFlags,
+    private val zenController: KeyguardZenAlarmViewController,
+    private val mediaController: KeyguardMediaViewController,
+) {
+    init {
+        if (!featureFlags.isSmartspaceEnabled()) {
+            context.packageManager.setComponentEnabledSetting(ComponentName("com.android.systemui", "com.lighthouse.android.systemui.keyguard.KeyguardSliceProviderLighthouse"), 1, 1)
+        } else {
+            mediaController.init()
+            zenController.init()
+        }
+    }
+}